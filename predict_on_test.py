1e2bc56a37aa4a0c076ccd2b338eb3862b678964
Dazhi Wang <ch2ohch2oh@gmail.com>
2021-04-12 23:15:11 -0400

    Add script to predict on test set

---
+++ predict_on_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+import sklearn
+import pandas as pd
+import pdb
+import os
+import sys
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+from tqdm import tqdm
+
+from tensorflow import keras
+
+df = pd.read_csv('test.csv')
+print(f'prog fraction = {df.target.mean()}')
+
+model = keras.models.load_model('dnn-v0.h5')
+df['pred'] = model.predict(df.iloc[:, 3:])
+res = df.groupby(by=['filename', 'target'], as_index=False)['pred'].mean()
+res['pred_class'] = (res.pred > 0.5)
+res['pred_class'] = res['pred_class'].astype(int)
+# print(res.head())
+print(f'acc = {np.mean(res.pred_class == res.target)}')
